6013fe341ee3a5d1ea626e6a31070b0576a73bfb
Tobias Silvestri <tobi.silvestri1@gmail.com>
2022-11-29 23:08:51 -0300

    primera pre-entrega

---
+++ index.js
@@ -0,0 +1,52 @@
+function promediar(num1, num2, num3, num4, num5){
+    let promedio = (num1 + num2 + num3 + num4 + num5)/5;
+    let mensaje = `El promedio de edad de la gente que ingreso a la web es de: ${promedio}`;
+}
+
+
+let num1 = parseInt(prompt("ingrese su edad"));
+if (num1 > 17){
+    alert("eres mayor de edad, continua con la encuenta")
+}else{
+    alert("eres menor de edad, recomendamos que llame a un adulto para seguir")
+}
+
+let num2 = parseInt(prompt("ingrese su edad"));
+if (num1 > 17){
+    alert("eres mayor de edad, continua con la encuenta")
+}else{
+    alert("eres menor de edad, recomendamos que llame a un adulto para seguir")
+}
+
+let num3 = parseInt(prompt("ingrese su edad"));
+if (num1 > 17){
+    alert("eres mayor de edad, continua con la encuenta")
+}else{
+    alert("eres menor de edad, recomendamos que llame a un adulto para seguir")
+}
+
+let num4 = parseInt(prompt("ingrese su edad"));
+if (num1 > 17){
+    alert("eres mayor de edad, continua con la encuenta")
+}else{
+    alert("eres menor de edad, recomendamos que llame a un adulto para seguir")
+}
+
+let num5 = parseInt(prompt("ingrese su edad"));
+if (num1 > 17){
+    alert("eres mayor de edad, continua con la encuenta")
+}else{
+    alert("eres menor de edad, recomendamos que llame a un adulto para seguir")
+}
+
+promediar(num1, num2, num3, num4, num5);
+alert ("los datos fueron completados exitosamente")
+
+
+
+
+
+
+
+
+
